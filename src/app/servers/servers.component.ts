6f41d5fba96191fb312fb1bb2d1cb679f66b3778
robyy <roby.yan84@gmail.com>
2017-05-30 22:06:37 -0400

    component selector options

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
+  // selector: '[app-servers]', // just like css attribute selector
+  // selector: '.app-servers', // notice that id selector not supported, other pseudo selectors like a:hover also don't work
   selector: 'app-servers',
   template: `
     <app-server></app-server>
